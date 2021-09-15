2d4091b270f53b67c34d013bf1f69a457b4e95a2
Katia <k.demagkou@gmail.com>
2021-09-15 12:21:13 +0300

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,68 @@
-import { ShoppingBasket, ShoppingCart } from "@material-ui/icons";
+import {
+  ShoppingBasket,
+  ShoppingCartOutlined,
+  MenuOutlined,
+} from "@material-ui/icons";
+import React from "react";
 // import { Link } from "react-router-dom";
-import styled from "styled-components"
+import styled from "styled-components";
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import '../index.css'
+import "../index.css";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({ 
+  menuItem: {
+    background: "#A36833",
+  
+},
+
+});
+
+export default function NavBar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <ShoppingCartOutlined />
+        </Left>
+        <Center>
+          <Logo>CHAMISAL</Logo>
+          <LogoSmall>VINEYARDS</LogoSmall>
+        </Center>
+        <Right>
+          <MenuOutlined onClick={handleClick}/>
+          <Menu 
+            className="menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem className={(classes.menuItem)} onClick={handleClose}> <a href="/">home</a></MenuItem>
+            <MenuItem className={(classes.menuItem)} onClick={handleClose}><a href="/shop">shop</a></MenuItem>
+            <MenuItem className={(classes.menuItem)} onClick={handleClose}><a href="/login">login</a></MenuItem>
+            <MenuItem className={(classes.menuItem)} onClick={handleClose}><a href="/registration">registration</a></MenuItem>
+          </Menu>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+}
 
 const Container = styled.div`
-  height: 60px;
+  height: 70px;
   border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 `;
 
@@ -17,43 +74,39 @@ const Wrapper = styled.div`
 `;
 
 const Left = styled.div`
-  flex: 1;
-  display: flex;
   align-items: center;
+  cursor: pointer;
+  border-radius: 50%;
+  border: 2px solid black;
+  padding: 10px;
+  display: flex;
+  align-content: center;
+  justify-content: center;
 `;
 
-const Shop = styled.div`
-`
+const Shop = styled.div``;
 
 const Center = styled.div`
   flex: 1;
   text-align: center;
 `;
-
+const Logo = styled.h1`
+  font-family: "source_sans_proregular", sans-serif;
+  color: #404040;
+  font-weight: bold;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+`;
+const LogoSmall = styled.h3`
+  color: #808080;
+  font-size: small;
+  letter-spacing: 0.3em;
+`;
 const Right = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  max-width: 100%;
 `;
 
-const NavBar = () => {
-  return (
-    <Container>
-      <Wrapper>
-        <Left>
-          <Shop>
-            <ShoppingCart />
-          </Shop>
-          <a href="/">home</a>
-          <a href="/shop">shop</a>
-          <a href="/login">login</a>
-          <a href="/registration">registration</a>
-        </Left>
-        <Center></Center>
-        <Right></Right>
-      </Wrapper>
-    </Container>
-  )
-}
 
-export default NavBar
